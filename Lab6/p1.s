5fafb2f489b8d3b8739ac2fd64736d66cfdf7a8d
Haoyu Liu <haoyul4@linux-a2.ews.illinois.edu>
2020-10-01 13:53:50 -0500

    lab6-1

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -12,4 +12,29 @@
 # }
 .globl fill_array
 fill_array:
+    li $t0, 0
+    move $t1, $a0
+    for:
+        bge $t0, 6, end_for
+        srl $t1, $t1, 3
+        li $t4, 3
+        div $t0, $t4
+        mfhi $t5
+        bne $t5, 0, else
+        mul $t2, $t0, 4
+        add $t2, $t2, $a1
+        li $t6, 31
+        and $t3, $t1, $t6
+        sw $t3, 0($t2)
+        add $t0, $t0, 1
+        j for
+        else:
+            mul $t2, $t0, 4
+            add $t2, $t2, $a1
+            li $t6, 255
+            and $t3, $t1, $t6
+            sw $t3, 0($t2)
+            add $t0, $t0, 1
+            j for
+    end_for:         
     jr      $ra
